32bf003a23d9d20dad37bbb6b0c87158788f166e
GooRiOn <foreverframenet@gmail.com>
2019-01-17 21:34:41 +0100

    Added blazor app

---
+++ src/Governor/Governor.Shared/ServiceOptions.cs
@@ -0,0 +1,10 @@
+namespace Governor.Shared
+{
+    public class ServiceOptions
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Command { get; set; }
+        public string HostedUrl { get; set; }
+    }
+}
